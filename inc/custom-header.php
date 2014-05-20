97eb4e98c0d1ec60d08809675d3e940ae64561e0
Devin Price <devin@wptheming.com>
2014-05-20 12:55:44 -0500

    Set font sizes back one smaller.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -11,7 +11,7 @@
  */
 function currents_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'currents_custom_header_args', array(
-		'default-image'          => get_template_directory_uri() . '/images/mountain.jpg',
+		'default-image'          => get_template_directory_uri() . '/images/summit.jpg',
 		'default-text-color'     => 'ffffff',
 		'width'                  => 890,
 		'height'                 => 410,
@@ -21,10 +21,19 @@ function currents_custom_header_setup() {
 add_action( 'after_setup_theme', 'currents_custom_header_setup' );
 
 /**
- * Remove the admin screen for the header.
+ * Remove the header menu item from the appearance menu.
  * This feature will be handled entirely through the customizer.
  */
 function currents_custom_header_screen() {
 	remove_submenu_page( 'themes.php', 'custom-header' );
 }
 add_action( 'admin_menu', 'currents_custom_header_screen', 20 );
+
+/**
+ * Remove the header menu item from the admin bar.
+ */
+function currents_admin_bar_header_menu( $wp_admin_bar ) {
+	global $wp_admin_bar;
+	$wp_admin_bar->remove_menu( 'header' );
+}
+add_action( 'wp_before_admin_bar_render', 'currents_admin_bar_header_menu' );
