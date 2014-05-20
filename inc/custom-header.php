62f85d383b0f4f0d65d887e4bdbe0ddab78372d8
Devin Price <devin@wptheming.com>
2014-05-20 14:59:28 -0500

    Add additional header images and readme.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -8,6 +8,8 @@
 
 /**
  * Setup the WordPress core custom header feature.
+ *
+ * @since 1.0.0
  */
 function currents_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'currents_custom_header_args', array(
@@ -23,6 +25,8 @@ add_action( 'after_setup_theme', 'currents_custom_header_setup' );
 /**
  * Remove the header menu item from the appearance menu.
  * This feature will be handled entirely through the customizer.
+ *
+ * @since 1.0.0
  */
 function currents_custom_header_screen() {
 	remove_submenu_page( 'themes.php', 'custom-header' );
@@ -31,9 +35,34 @@ add_action( 'admin_menu', 'currents_custom_header_screen', 20 );
 
 /**
  * Remove the header menu item from the admin bar.
+ *
+ * @since 1.0.0
  */
 function currents_admin_bar_header_menu( $wp_admin_bar ) {
 	global $wp_admin_bar;
 	$wp_admin_bar->remove_menu( 'header' );
 }
 add_action( 'wp_before_admin_bar_render', 'currents_admin_bar_header_menu' );
+
+/**
+ * Remove the header menu item from the admin bar.
+ *
+ * @since 1.0.0
+ */
+register_default_headers( array(
+	'cloudscape' => array(
+		'url'           => '%s/images/cloudscape.jpg',
+		'thumbnail_url' => '%s/images/cloudscape.jpg',
+		'description'   => __( 'Cloudscape', 'currents' )
+	),
+	'dune' => array(
+		'url'           => '%s/images/dune.jpg',
+		'thumbnail_url' => '%s/images/dune.jpg',
+		'description'   => __( 'Dune', 'currents' )
+	),
+	'golden' => array(
+		'url'           => '%s/images/golden.jpg',
+		'thumbnail_url' => '%s/images/golden.jpg',
+		'description'   => __( 'Golden', 'currents' )
+	)
+) );
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Summit
+
+Summit is simple one-column WordPress theme, perfect for blogging and small business.  It features excellent header images with a number of customizable display options.  A social icon can be added to the header and there's multiple options for the widget area in the footer.
+
+## Credits
+
+WordPress projects always stand on the shoulders of others.  Please take this theme and build upon it for you own work!  This theme was based off [Underscores](http://underscores.me/) by Automattic.
+
+"Cloudscape" image by [Robin Benad](http://robinbenad.com/).
+
+"Dune" image by [Tim de Groot](http://timthing.me/).
+
+"Golden" image by [Linh Nguyen](http://www.ngkhanhlinh.com/).
+
+"Summit" image by [Thomas Haase](https://creativemarket.com/moonloop).
+
+Summit 1.0.0
+----
+
+* Public Release
